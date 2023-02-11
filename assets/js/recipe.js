d6a30c1002e0a358fcf313e7543dd6cc3be51149
jordshub <119757179+jordshub@users.noreply.github.com>
2023-02-11 18:08:20 +0000

    amended modals to open on card click, added hover affects, added placeholder giphy

--- assets/js/recipe.js
+++ assets/js/recipe.js
@@ -1,9 +1,9 @@
-// $( document ).ready() {
+// $(document).ready()
 
-  let formSearch = $('#search-form');
+let formSearch = $('#search-form');
 
 // api kiey for spoontacular api
-const apiKey = '&apiKey=c0fcbd7c25a14b24b516d287693d9360';
+const apiKey = '&apiKey=7f77bc0ac3a540babaab9a36bfd949e8';
 
 const apiKey2 = 'k1qbPQr1VChVz4qnj3hhtTFNLs1CGc6T';
 
@@ -28,67 +28,67 @@ function getResponse(search) {
   });
 }
 
-  // function to obtain api request results and display them on the screen
-  function displayRecipe(response){
-    // for loop to iterate through results array 
-    for(let i = 0; i < 9; i++) {
-      // create HTML elements an assign to a variable
-        // let imgEl = $('<img>');
-        // let foodType = $('<div>');
+// function to obtain api request results and display them on the screen
+function displayRecipe(response) {
+  // for loop to iterate through results array 
+  for (let i = 0; i < 9; i++) {
+    // create HTML elements an assign to a variable
+    // let imgEl = $('<img>');
+    // let foodType = $('<div>');
 
-      // create IDs to assign to HTML elements
-        let imageID = 'img' + i;
-        let titleID = 'title' + i;
+    // create IDs to assign to HTML elements
+    let imageID = 'img' + i;
+    let titleID = 'title' + i;
 
-      // grab the recipe image and titile and assign to a variable
-        let image = response.results[i].image;
-        let title = response.results[i].title;
+    // grab the recipe image and titile and assign to a variable
+    let image = response.results[i].image;
+    let title = response.results[i].title;
 
-        // set value of created ID
-        // imgEl.text(image);
-        $('#' + titleID).text(title);
+    // set value of created ID
+    // imgEl.text(image);
+    $('#' + titleID).text(title);
 
-      // add attributes to elements
-        $('#' + imageID).attr('src', image);
+    // add attributes to elements
+    $('#' + imageID).attr('src', image);
 
-        // $('body').append(imgEl)
-        // $('body').append(foodType)
-    }
+    // $('body').append(imgEl)
+    // $('body').append(foodType)
   }
+}
 
-  // run an API request to retrieve 3 random recipes to display when the page loads 
-  function randomRecipes() {
-    let randomRecipeURL = 'https://api.spoonacular.com/recipes/random?number=3' + apiKey;
+// run an API request to retrieve 3 random recipes to display when the page loads 
+function randomRecipes() {
+  let randomRecipeURL = 'https://api.spoonacular.com/recipes/random?number=3' + apiKey;
 
-    $.ajax({
-      url: randomRecipeURL,
-      method: "GET"    
-    }).then(function(response) {
-      
-      displayRandomRecipes(response);
-    });
+  $.ajax({
+    url: randomRecipeURL,
+    method: "GET"
+  }).then(function (response) {
+
+    displayRandomRecipes(response);
+  });
+}
+
+function displayRandomRecipes(response) {
+  console.log(response);
+  for (let i = 9; i < 12; i++) {
+    // create IDs to assign to HTML elements
+    let imageID = 'img' + i;
+    let titleID = 'title' + i;
+
+    // grab the recipe image and titile and assign to a variable
+    let image = response.recipes[i].image;
+    let title = response.recipes[i].title;
+
+    // set value of created ID        
+    $('#' + titleID).text(title);
+
+    // add attributes to elements
+    $('#' + imageID).attr('src', image);
   }
+}
 
-  function displayRandomRecipes(response) {
-    console.log(response);  
-    for(let i = 9; i < 12; i++) {      
-      // create IDs to assign to HTML elements
-        let imageID = 'img' + i;
-        let titleID = 'title' + i;
-
-      // grab the recipe image and titile and assign to a variable
-        let image = response.recipes[i].image;        
-        let title = response.recipes[i].title;        
-
-      // set value of created ID        
-        $('#' + titleID).text(title);
-
-      // add attributes to elements
-        $('#' + imageID).attr('src', image);
-    }
-  }
-
-  // randomRecipes();
+// randomRecipes();
 
 function displayGiphy() {
   fetch(queryURL2)
@@ -107,15 +107,15 @@ function displayGiphy() {
 
 $("#search-form").on('submit', function (event) {
   event.preventDefault();
-  
+
   // let search = $('#search-form').val().trim();
   let search = $('#search').val().trim();
   console.log(typeof search);
   console.log('search: ' + search);
 
-    formSearch.val('');
+  formSearch.val('');
 
-    // getResponse(search);
+  getResponse(search);
 
-  // displayGiphy()
+  displayGiphy()
 });
